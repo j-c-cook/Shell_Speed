9e4af1d49eecdee4f9e7e3b0c63f0a49411d36ad
j-c-cook <jack.cook@okstate.edu>
2020-12-24 23:13:01 -0600

    bash file for transferring data to remote directory via ssh, scp

---
+++ To_Cluster.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Jack C. Cook
+# Thursday, December 24, 2020
+# To_Cluster.sh - Transfer files to cluster via scp
+# History: Goes back many months, just now cleaning up for teammates to use 
+
+# Source the configuration file to collect all of the necessary variables 
+source configuration.dat 
+
+# Request an input from the user for the cluster to be transfering files to 
+read -p "Enter Name of Cluster (Cowboy/Pete/Raspy): " clusterName
+
+# if the files are to be transferred to Cowboy
+if [ "$clusterName" == "Cowboy" -o "$clusterName" == "C" -o "$clusterName" == "c" ]; then 
+	echo "The files will be transferred to Cowboy."
+	loc="$Cowboy_UserName@cowboy.hpc.okstate.edu"
+	name="Cowboy"
+# if the files are to be transferred to Pete
+elif [ "$clusterName" == "Pete"  -o "$clusterName" == "P" -o "$clusterName" == "p" ]; then
+	echo "The files will be transferred to Pete."
+	loc="$Pete_UserName@pete.hpc.okstate.edu"
+	name="Pete"
+else 
+	# For information on coloring text: https://stackoverflow.com/a/28938235/11637415
+	Red='\033[0;31m' 
+	echo -e "${Red}$clusterName is unavailable, please provide one of the following inputs: c, p, r" 
+	exit 1
+fi
+
+# Reqeust input for the folder the files will be moved into
+read -p "Enter file path for $name destination: " path_to_folder
+echo "The file(s) will be moved to $path_to_folder on $name"
+
+echo "The location is $loc"
+
+# could loop through the files and folders, but put them all on one line
+echo "${files_folders[@]}"
+
+# Change one directory down to get into other submodule
+cd ..
+
+scp -r "${files_folders[@]}" $loc:$path_to_folder
